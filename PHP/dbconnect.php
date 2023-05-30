94d9ae96f7842bd5b9596ae8c6c1b9423fed41c7
velican <120579492+ashborn211@users.noreply.github.com>
2023-05-30 12:38:57 +0200

    dit is gewoon het begin we gaan nog veel doen

---
+++ PHP/dbconnect.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+$servername = 'localhost';
+$database = 'travel-menu';
+$user = 'velican';
+$pass = 'velican009';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$servername;dbname=$database;charset=$charset";
+
+try
+{
+    // set the PDO error mode to exception
+$connect = new PDO($dsn, $user, $pass);
+$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// echo "verbinding is gemaakt.";
+}
+
+catch (PDOException $e) 
+{
+echo $e->getMessage();
+die("Sorry database probleem");
+
+}
+?>
---
+++ PHP/login-register/login.php
@@ -0,0 +1,33 @@
+<?php
+require_once('../dbconnect.php');
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../../CSS/style.css" />
+    <title>Home</title>
+  </head>
+  <body>
+    <header>
+        <img src="../../IMG/pngwing.com.png" alt="Home"
+      /></a>
+      <nav class="top-bar">
+        <div>All flights</div>
+        <div>Schedule</div>
+        <div>Transport and directions</div>
+      </nav>
+    </header>
+    <main>
+
+        <form action="" method="post">
+            <input type="email" name="email" id="" placeholder="email">
+            <input type="password" name="password" id="">
+            <input type="submit" value="login">
+        </form>
+    </main>
+  </body>
+</html>
