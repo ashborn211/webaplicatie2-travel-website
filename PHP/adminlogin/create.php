96be222d6a7f45b6a19266181fa2756a8472e230
velican <120579492+ashborn211@users.noreply.github.com>
2023-06-02 16:49:38 +0200

    made a confirm butten and made create

---
+++ PHP/adminlogin/create.php
@@ -0,0 +1,63 @@
+<?php
+
+if(isset($_POST["submit"])){
+    $countryname = $_POST['countryname'];
+   
+    $sql = "INSERT INTO country (name)
+            VALUES (:countryname)";
+
+    $stmt = $connect->prepare($sql);
+    $stmt->bindParam(":countryname", $_POST['countryname']);      
+    $stmt->execute();
+    header("location: create.php");
+
+    $placename = $_POST['placename'];
+    $countryid = $_POST['countryid'];
+   
+    $sql = "INSERT INTO places (name, countryid)
+            VALUES (:placename, :countryid,)";
+
+    $stmt = $connect->prepare($sql);
+    $stmt->bindParam(":countryid", $_POST['countryid']);     
+    $stmt->bindParam(":name", $_POST['placename']);     
+    $stmt->execute();
+    header("location: create.php");
+
+}
+
+    ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../../CSS/style.css" />
+    <script src="../../JS/confirm.js"></script>
+    <title>Home</title>
+  </head>
+  <body>
+    <header>
+        <img src="../../IMG/pngwing.com.png" alt="Home"
+      /></a>
+      <nav class="top-bar">
+        <div>All flights</div>
+        <div>Schedule</div>
+        <div>Transport and directions</div>
+      </nav>
+    </header>
+    <main>
+
+        <form action="" method="post">
+            <input type="countryname" name="countryname" id="" >
+            <input type="placename" name="placename" id="" >
+            <input type="countryid" name="countryid" id="">
+            <input type="submit" value="login" onClick='return confirmSubmit()'>
+        </form>
+
+    </main>
+  </body>
+</html>
+
--- PHP/login-register/loginhelper.php
+++ PHP/login-register/loginhelper.php
@@ -1,35 +1,42 @@
 <?php
 include_once('../dbconnect.php');
 var_dump($_POST);
-
-
-$sql = "SELECT username, password, emailadress FROM users ORDER BY id ASC";
-$stmt = $connect->prepare($sql);
-$stmt -> FetchAll(PDO::FETCH_ASSOC);
-$stmt->execute();
-$result = $stmt ->FetchAll(PDO::FETCH_ASSOC);
-
-
 if(isset($_POST['username']) && isset($_POST['password']) && $_POST(["emailadress"])){
-    if($_POST["username"] == '' && 
-    $_POST["password"] == '' && 
-    $_POST["emailadress"]){
-        echo 'successfuly logged in';
-        echo 'welcome';
-    } else{
-        if(isset($_POST['username']) && isset($_POST['password']) && $_POST(["emailadress"])){
-            var_dump($_POST);
-            if($_POST['username'] == ""){
-                echo 'email can\'t be empty <br>';
+    
+    $sql = "SELECT username, password, emailadress FROM users ORDER BY id ASC";
+    $stmt = $connect->prepare($sql);
+    $stmt -> FetchAll(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $result = $stmt ->FetchAll(PDO::FETCH_ASSOC);
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $emailadress = $_POST['emailadress'];
+    
+    foreach($result as $data) {
+        if($_POST["username"] == $username && 
+        $_POST["password"] == $password && 
+        $_POST["emailadress"] == $emailadress){
+            echo 'successfuly logged in';
+            echo 'welcome';
+        } else{
+            if(isset($_POST['username']) && isset($_POST['password']) && $_POST(["emailadress"])){
+                var_dump($_POST);
+                if($_POST['username'] == ""){
+                    echo 'email can\'t be empty <br>';
+                }
+                if($_POST['password'] == ""){
+                    echo 'password can\'t be empty <br>';
+                }
+                if($_POST['emailadress'] == ""){
+                    echo 'password can\'t be empty <br>';
+                }
             }
-            if($_POST['password'] == ""){
-                echo 'password can\'t be empty <br>';
-            }
-            if($_POST['emailadress'] == ""){
-                echo 'password can\'t be empty <br>';
-            }
+           
         }
-       
+
     }
+    
+    
     }
     ?>
