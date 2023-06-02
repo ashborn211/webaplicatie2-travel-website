2a7642abf59fb54ebe7e41fbf85dea729b13cce1
velican <120579492+ashborn211@users.noreply.github.com>
2023-06-02 15:09:37 +0200

    heb een register gemaakt en bezig met login page

--- PHP/login-register/register.php
+++ PHP/login-register/register.php
@@ -1,5 +1,21 @@
 <?php
 require_once('../dbconnect.php');
+
+if(isset($_POST["submit"])){
+    $username = $_POST['username'];
+    $emailadress = $_POST['emailadress'];
+    $password = $_POST['password'];
+   
+    $sql = "INSERT INTO users (username, emailadress, password)
+            VALUES (:username, :emailadress, :password)";
+
+    $stmt = $connect->prepare($sql);
+    $stmt->bindParam(":username", $_POST['username']);     
+    $stmt->bindParam(":emailadress", $_POST['emailadress']);     
+    $stmt->bindParam(":password", $_POST['password']);     
+    $stmt->execute();
+    header("location: register.php");
+}
 ?>
 <!DOCTYPE html>
 
@@ -24,12 +40,12 @@ require_once('../dbconnect.php');
     <main>
 
         <form action="" method="post">
-            <input type="username" name="email" id="" >
+        <input type="username" name="email" id="" >
             <input type="emailadress" name="emailadress" id="" >
             <input type="password" name="password" id="">
             <input type="submit" value="login">
         </form>
-
+        
     </main>
   </body>
 </html>
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # webaplicatie2-travel-website
+
+ik(ashborn211) doe de PHP JS en database
+homelessman3000 doed de html en css
