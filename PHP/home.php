cc821370ac765e9e9e8605e45c6bc4737aa1a07a
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2023-06-09 14:01:36 +0200

    new changes login

--- PHP/home.php
+++ PHP/home.php
@@ -18,7 +18,7 @@
         <div>Transport and directions</div>
       </nav>
       <h1 class="quo">Ready To Fly?</h1>
-      <button class="login-page"><link rel="stylesheet" href="Login.html">Login</button>
+      <a class="login-page" href="login.php"></a>
     </header>
     <main></main>
   </body>
