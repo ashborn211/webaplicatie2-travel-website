6b2f43d32397e95205496c7b6e4fc82138d5aa49
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2023-06-13 10:56:27 +0200

    login completed

--- PHP/login.php
+++ PHP/login.php
@@ -20,7 +20,46 @@
         />
       </div>
       <div class="input-field">
+        <label for="password">Passwo<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="../CSS/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+  </head>
+  <header>
+  <body>
+    <a href="/PHP/Home.html"
+    ><img src="../IMG/pngwing.com.png" alt="Home"
+  /></a>
+  </header>
+    <div class="login-box">
+      <h2>Login</h2>
+      <div class="input-field">
+        <label for="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div class="input-field">
         <label for="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+      </div>
+      <button type="submit">Login</button>
+    </div>
+  </body>
+</html>
+rd:</label>
         <input
           type="password"
           id="password"
