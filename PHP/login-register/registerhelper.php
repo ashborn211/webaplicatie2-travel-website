abcc7f7864399c7bd47d4adb74a6ec97551f6aea
velican <120579492+ashborn211@users.noreply.github.com>
2023-06-16 15:01:01 +0200

    heb register helper een beetje verrandert

--- PHP/login-register/registerhelper.php
+++ PHP/login-register/registerhelper.php
@@ -1,24 +1,16 @@
 <?php
 include_once('../dbconnect.php');
 var_dump($_POST);
-
-$data = $connect->query("SELECT * FROM users")->fetchAll(); 
-
-foreach ($data as $row){
-    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['emailadress'])){
- 
-            if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['emailadress'])){
-                var_dump($_POST);
-                if($_POST['username'] == ""){
-                    echo 'username can\'t be empty <br>';
-                }
-                if($_POST['password'] == ""){
-                    echo 'password can\'t be empty <br>';
-                }
-                if($_POST['emailadress'] == ""){
-                    echo 'emailadress can\'t be empty <br>';
-                }
+    if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['emailadress'])){
+        var_dump($_POST);
+        if($_POST['username'] == ""){
+            echo 'username can\'t be empty <br>';
+           }
+        if($_POST['password'] == ""){
+            echo 'password can\'t be empty <br>';
             }
-    }
-}   
+        if($_POST['emailadress'] == ""){
+            echo 'emailadress can\'t be empty <br>';
+        }
+    }   
 ?>
