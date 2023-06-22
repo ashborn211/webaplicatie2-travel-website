d6a08c155d6c92a4035316be5ef4940e678d3dab
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2023-06-22 09:21:25 +0200

    New

--- PHP/login.php
+++ PHP/login.php
@@ -7,28 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
   </head>
-  <body>
-    <div class="login-box">
-      <h2>Login</h2>
-      <div class="input-field">
-        <label for="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email"
-        />
-      </div>
-      <div class="input-field">
-        <label for="password">Passwo<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="../CSS/style.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
-  </head>
   <header>
   <body>
     <a href="/PHP/Home.html"
@@ -48,18 +26,6 @@
       </div>
       <div class="input-field">
         <label for="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-      </div>
-      <button type="submit">Login</button>
-    </div>
-  </body>
-</html>
-rd:</label>
         <input
           type="password"
           id="password"
